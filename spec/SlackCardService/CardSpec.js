8d7dfc4a120772e1cf0bc74a25817de70f67c4ee
Sean-Brown <smbrown3706@gmail.com>
2016-02-05 10:00:10 -0500

    Remove *.js and *.js.map files from the spec directory

--- spec/SlackCardService/CardSpec.js
+++
@@ -1,8 +0,0 @@
-var card_1 = require("../../card_service/base_classes/items/card");
-describe("Test the BaseCard", function () {
-    it("should print its name correctly", function () {
-        var card = new card_1.BaseCard(card_1.Suit.Spades, card_1.Value.Ace);
-        expect(card.toString()).toEqual("Ace of Spades");
-    });
-});
-//# sourceMappingURL=CardSpec.js.map
