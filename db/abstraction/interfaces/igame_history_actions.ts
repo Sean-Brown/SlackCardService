a981e76801990d4ebff21e62524766ec35eb20eb
Sean Brown <smbrown3706@gmail.com>
2016-10-04 16:42:46 -0400

    Got rid of Promise in favor of Q promises. Added a method endGame() to update a game_history row's "ended" timestamp.

--- db/abstraction/interfaces/igame_history_actions.ts
+++ db/abstraction/interfaces/igame_history_actions.ts
@@ -3,13 +3,21 @@ export interface IGameHistoryActions {
     /**
      * Create a game history record
      * @param game_id
-     * @return {GameHistoryReturn} the newly created row
+     * @return {Q.Promise<GameHistoryReturn>} the newly created row
      */
-    create(game_id:number):JQueryPromise<GameHistoryReturn>;
+    create(game_id:number):Q.Promise<GameHistoryReturn>;
 
     /**
-     * Find the most recent game history
+     * Find the most recent game history that has not ended
      * @param game_id the ID of the game
+     * @return {Q.Promise<GameHistoryReturn>} the found row
      */
-    findMostRecent(game_id:number):JQueryPromise<GameHistoryReturn>;
+    findMostRecent(game_id:number):Q.Promise<GameHistoryReturn>;
+
+    /**
+     * End this game-history record (i.e. set the 'ended' date)
+     * @param game_history_id
+     * @return {Q.Promise<GameHistoryReturn>} the updated row
+     */
+    endGame(game_history_id:number):Q.Promise<GameHistoryReturn>;
 }
