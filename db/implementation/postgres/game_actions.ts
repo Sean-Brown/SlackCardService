5267124d18283cafa3b2de02ee7a555fa4058ce0
Sean Brown <smbrown3706@gmail.com>
2016-10-04 12:21:53 -0400

    Ditch Promise (which has crappy Typescript support) in favor of Q. Remove processResult() -- leave processing the result to the caller. Don't export the GameActions class, instead export a game_actions singleton. Fixed a bug that wasn't caught by WebStorm where runQueryReturning was resolving a PGQueryReturn object (as opposed to the promised GameReturn object).

--- db/implementation/postgres/game_actions.ts
+++ db/implementation/postgres/game_actions.ts
@@ -1,72 +1,44 @@
+/// <reference path="../../../typings/index.d.ts" />
+
 import {IGameActions} from "../../abstraction/interfaces/igame_actions";
 import {GameReturn, DBReturnStatus} from "../../abstraction/return/db_return";
 import {getTableName, DBTables} from "../../abstraction/tables/base_table";
-import Promise = require("Promise");
 import {QueryResult} from "pg";
-import {pg_mgr} from "./manager";
+import {pg_mgr, PGQueryReturn} from "./manager";
 import {Game} from "../../abstraction/tables/game";
+var Q = require("q");
 
 export module PostgresImpl {
-    export class GameActions implements IGameActions {
-        runQueryReturning(query:string):Promise<GameReturn> {
-            return new Promise<GameReturn>(function(resolve, reject) {
-                pg_mgr.query(query, null, (err: Error, result: QueryResult) => {
-                    var ret = new GameReturn();
-                    if (err != null) {
-                        ret.status = DBReturnStatus.error;
-                        ret.message = err.message;
-                    }
-                    else if (result.rowCount > 0) {
-                        ret.result.push(new Game(result.rows[0][0], result.rows[0][1]))
-                    }
-                    resolve(ret);
-                });
+    class GameActions implements IGameActions {
+        runQueryReturning(query:string):Q.Promise<GameReturn> {
+            return new Q.Promise((resolve) => {
+                var ret = new GameReturn();
+                pg_mgr.runQuery(query)
+                    .then((result:PGQueryReturn) => {
+                        ret.initFromResult(result);
+                        resolve(ret);
+                    });
             });
         }
-        processResult(result: GameReturn) {
-            if (result.status != DBReturnStatus.ok) {
-                throw result.message;
-            }
-            else if (result.result.length == 0) {
-                throw "did not get back any rows";
-            }
-        }
-        create(name:string):GameReturn {
-            var ret = new GameReturn();
+        public create(name:string):Q.Promise<GameReturn> {
             var query = `
                 INSERT INTO ${getTableName(DBTables.Game)} (name) VALUES 
-                (${name}) RETURNING *;
+                ('${name}') RETURNING *;
             `.trim();
-            var that = this;
-            this.runQueryReturning(query).then(function(result: GameReturn) {
-                that.processResult(result);
-                ret = result;
-            });
-            return ret;
+            return this.runQueryReturning(query);
         }
-        findByName(name:string):GameReturn {
-            var ret = new GameReturn();
+        public findByName(name:string):Q.Promise<GameReturn> {
             var query = `
                 SELECT * FROM ${getTableName(DBTables.Game)} WHERE name='${name}' LIMIT 1;
             `.trim();
-            var that = this;
-            this.runQueryReturning(query).then(function(result: GameReturn) {
-                that.processResult(result);
-                ret = result;
-            });
-            return ret;
+            return this.runQueryReturning(query);
         }
-        find(id:number):GameReturn {
-            var ret = new GameReturn();
+        public find(id:number):Q.Promise<GameReturn> {
             var query = `
                 SELECT * FROM ${getTableName(DBTables.Game)} WHERE id=${id} LIMIT 1;
             `.trim();
-            var that = this;
-            this.runQueryReturning(query).then(function(result: GameReturn) {
-                that.processResult(result);
-                ret = result;
-            });
-            return ret;
+            return this.runQueryReturning(query);
         }
     }
+    export var game_actions = new GameActions();
 }
