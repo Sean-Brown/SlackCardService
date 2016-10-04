8ce936feaacb85e542a3775fbb09eb45814580cb
Sean Brown <smbrown3706@gmail.com>
2016-10-04 16:44:27 -0400

    Uncomment 'received' attribute. Added a 'cut' attribute to track what the cut card was that hand.

--- db/abstraction/tables/hand_history.ts
+++ db/abstraction/tables/hand_history.ts
@@ -13,7 +13,7 @@ export class HandHistory extends BaseTable {
     game_history_id:number;
 
     /**
-     * The player's hand
+     * The player's hand EXCLUDING the cut card
      * This is a hyphen-delimited shorthand form of the hand, e.g.
      * 7h-3s-10c-kh-qd for "seven of hearts"-"three of spades"-
      * "ten of clubs"-"king of hearts"-"queen of diamonds"
@@ -21,16 +21,22 @@ export class HandHistory extends BaseTable {
     hand:string;
 
     /**
+     * The cut card in short form, e.g. 5c for "five of clubs"
+     */
+    cut:string;
+
+    /**
      * Date this hand was received, in the form of a number
      * Automatic
      */
-    //received:number;
+    received:number;
 
-    constructor(id:number, player_id:number, game_history_id:number, hand:string="") {
+    constructor(id:number, player_id:number, game_history_id:number, hand:string="", cut:string="") {
         super(id);
         this.player_id = player_id;
         this.game_history_id = game_history_id;
         this.hand = hand;
+        this.cut = cut;
     }
 
     getTable():DBTables {
