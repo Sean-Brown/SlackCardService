7532fbd7812aa9f6e3cf2c13fe3ee7f9d72b97a9
Sean Brown <smbrown3706@gmail.com>
2016-10-06 09:28:59 -0400

    Modified the express app creation to return a promise, this will allow for the database to be created and populated before running Express.

--- spec/Node/setup.ts
+++ spec/Node/setup.ts
@@ -1,9 +1,11 @@
 /// <reference path="../../typings/index.d.ts" />
-/// <reference path="../../typings/index.d.ts/mocha/mocha.d.ts" />
 
 import {setup} from "../../app";
 import {Express} from "express";
 import * as Mocha from "mocha";
+import {readConfigFromEnv} from "../db/postgres/integration/setEnv";
+
+var Q = require("q");
 
 var express = require("express");
 
@@ -11,6 +13,17 @@ interface TestClass extends Mocha.ITest {
     app: Express;
 }
 
-export function createNewServer(test: TestClass): void {
-    test.app = setup(express());
+export function createNewServer(test: TestClass): Q.Promise<void> {
+    return new Q.Promise((resolve, reject) => {
+        readConfigFromEnv();
+        setup(express())
+            .then((app:Express) => {
+                test.app = app;
+                resolve();
+            })
+            .catch(() => {
+                test.app = null;
+                reject();
+            });
+    });
 }
