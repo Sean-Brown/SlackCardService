f5d92b06baa7b6b53178dd2046bee552fe828c81
Sean Brown <smbrown3706@gmail.com>
2016-10-06 09:16:00 -0400

    Added cribbage_hand_history_actions to derive from the abstract hand_history table, this new class contains the cut card

---
+++ db/implementation/postgres/cribbage_hand_history_actions.ts
@@ -0,0 +1,27 @@
+import {ICribbageHandHistoryActions} from "../../abstraction/interfaces/icribbage_hand_history_actions";
+import {CribbageHandHistoryReturn} from "../../abstraction/return/db_return";
+import {pg_mgr, PGQueryReturn} from "./manager";
+import {DBTables, getTableName} from "../../abstraction/tables/base_table";
+var Q = require("q");
+
+class CribbageHandHistoryActions implements ICribbageHandHistoryActions {
+    private runQueryReturning(query:string):Q.Promise<CribbageHandHistoryReturn> {
+        return new Q.Promise((resolve) => {
+            var ret = new CribbageHandHistoryReturn();
+            pg_mgr.runQuery(query)
+                .then((result:PGQueryReturn) => {
+                    ret.initFromResult(result);
+                    resolve(ret);
+                });
+        });
+    }
+    create(player_id:number, game_history_id:number, hand:string, cut:string):Q.Promise<CribbageHandHistoryReturn> {
+        var query = `
+            INSERT INTO ${getTableName(DBTables.CribbageHandHistory)} (player_id, game_history_id, hand, cut) 
+            VALUES (${player_id}, ${game_history_id}, '${hand}', '${cut}') 
+            RETURNING *;
+        `.trim();
+        return this.runQueryReturning(query);
+    }
+}
+export var cribbage_hand_history_actions = new CribbageHandHistoryActions();
