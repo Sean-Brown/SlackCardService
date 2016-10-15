e4838146e293a9897ad8de0d8e2fb240d2ecaa12
Sean Brown <smbrown3706@gmail.com>
2016-10-15 12:08:43 -0400

    Added a catch to createCribbageHandHistory(). Added tests for the foreign key constraints. Removed the 'deleteTables()' part of the beforeEach().

--- spec/db/postgres/integration/CribbageHandHistoryActionsSpec.ts
+++ spec/db/postgres/integration/CribbageHandHistoryActionsSpec.ts
@@ -7,19 +7,25 @@ import {readConfigFromEnv} from "./setEnv";
 import {createGame, verifyReturn} from "./GameActionsSpec";
 import {createPlayer} from "./PlayerActionsSpec";
 import {createGameHistory} from "./GameHistoryActionsSpec";
-import {HandHistory} from "../../../../db/abstraction/tables/hand_history";
 import {cribbage_hand_history_actions} from "../../../../db/implementation/postgres/cribbage_hand_history_actions";
 import {CribbageHandHistoryReturn} from "../../../../db/abstraction/return/db_return";
+import {CribbageHandHistory} from "../../../../db/abstraction/tables/cribbage_hand_history";
+import {Cribbage} from "../../../../card_service/implementations/cribbage";
 var Q = require("q");
 
 const hand = "4s-4c-5d-6s";
 const cut = "6h";
-function createHandHistory(player_id:number, game_history_id:number): Q.Promise<HandHistory> {
-    return new Q.Promise((resolve) => {
-        cribbage_hand_history_actions.create(player_id, game_history_id, hand, cut).then((ret: CribbageHandHistoryReturn) => {
-            verifyReturn(ret, "Expected a hand-history result");
-            resolve(ret.first());
-        })
+function createCribbageHandHistory(player_id:number, game_history_id:number): Q.Promise<CribbageHandHistory> {
+    return new Q.Promise((resolve, reject) => {
+        cribbage_hand_history_actions.create(player_id, game_history_id, hand, cut)
+            .then((ret: CribbageHandHistoryReturn) => {
+                verifyReturn(ret, "Expected a hand-history result");
+                resolve(ret.first());
+            })
+            .catch((ret: CribbageHandHistoryReturn) => {
+                expect(ret.first()).toBeNull("No result should have been returned.");
+                reject(null);
+            });
     });
 }
 describe("Test the 'hand-history' actions", function() {
@@ -28,32 +34,50 @@ describe("Test the 'hand-history' actions", function() {
     var gameHistory:GameHistory = null;
     beforeEach(function(done) {
         readConfigFromEnv();
-        // Asynchronously drop the schema
-        deleteTables().then(() => {
-            // Re-create the tables to start from a fresh slate
-            PostgresTables.createTables()
-                .then(() => {
-                    return createGame();
-                })
-                .then((result:Game) => {
-                    game = result;
-                    return createPlayer();
-                })
-                .then((result:Player) => {
-                    player = result;
-                    return createGameHistory(game.id);
-                })
-                .then((result:GameHistory) => {
-                    gameHistory = result;
-                })
-                .finally(() => { done(); });
-        });
+        // Create the tables
+        PostgresTables.createTables()
+            .then(() => {
+                return createGame();
+            })
+            .then((result:Game) => {
+                game = result;
+                return createPlayer();
+            })
+            .then((result:Player) => {
+                player = result;
+                return createGameHistory(game.id);
+            })
+            .then((result:GameHistory) => {
+                gameHistory = result;
+            })
+            .finally(() => { done(); });
     });
     afterEach(function(done) {
         // Drop the tables
         deleteTables().finally(() => { done(); });
     });
     it("can add a hand-history row", function(done) {
-        createHandHistory(player.id, gameHistory.id).finally(() => { done(); });
+        createCribbageHandHistory(player.id, gameHistory.id)
+            .catch(() => {
+                // make the test fail
+                expect(true).toBeFalsy(`Test should have succeeded`);
+            })
+            .finally(() => { done(); });
+    });
+    it("enforces player_id foreign key constraints", function(done) {
+        createCribbageHandHistory(0, gameHistory.id)
+            .then(() => {
+                // make the test fail
+                expect(true).toBeFalsy(`Test should have failed`);
+            })
+            .finally(() => { done(); });
+    });
+    it("enforces game_history_id foreign key constraints", function(done) {
+        createCribbageHandHistory(player.id, 0)
+            .then(() => {
+                // make the test fail
+                expect(true).toBeFalsy(`Test should have failed`);
+            })
+            .finally(() => { done(); });
     });
 });
