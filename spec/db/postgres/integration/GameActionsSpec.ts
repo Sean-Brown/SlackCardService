6c287b8acc8084b4b393ec30cfdc148be7ae512c
Sean Brown <smbrown3706@gmail.com>
2016-10-15 12:09:24 -0400

    Added catch clauses to that will fail the test if an error occurred when a success was expected. Removed the 'deleteTables()' part of the beforeEach().

--- spec/db/postgres/integration/GameActionsSpec.ts
+++ spec/db/postgres/integration/GameActionsSpec.ts
@@ -15,11 +15,16 @@ export const game = "cribbage";
  * @returns the Game object for that row
  */
 export function createGame(): Q.Promise<Game> {
-    return new Q.Promise((resolve) => {
-        game_actions.create(game).then((ret: GameReturn) => {
-            verifyReturn(ret, "Expected a result from creating a game");
-            resolve(ret.first());
-        });
+    return new Q.Promise((resolve, reject) => {
+        game_actions.create(game)
+            .then((ret: GameReturn) => {
+                verifyReturn(ret, "Expected a result from creating a game");
+                resolve(ret.first());
+            })
+            .catch((ret: GameReturn) => {
+                expect(ret.first).toBeNull("Should have returned a null result");
+                reject(null);
+            });
     });
 }
 export function verifyReturn<ReturnType>(ret:DBReturn<ReturnType>, resultIsNullMsg:string) {
@@ -29,20 +34,19 @@ export function verifyReturn<ReturnType>(ret:DBReturn<ReturnType>, resultIsNullM
 describe("Test the 'game' actions", function() {
     beforeEach(function(done) {
         readConfigFromEnv();
-        // Asynchronously drop the schema
-        deleteTables()
-            .then(() => {
-                // Re-create the tables to start from a fresh slate
-                return PostgresTables.createTables();
-            })
-            .finally(() => { done(); });
+        PostgresTables.createTables().finally(() => { done(); });
     });
     afterEach(function(done) {
         // Drop the tables
         deleteTables().finally(() => { done(); });
     });
     it("can create a new game entry", function(done) {
-        createGame().finally(() => { done(); });
+        createGame()
+            .catch(() => {
+                // fail the test
+                expect(true).toBeFalsy("Test should have succeeded");
+            })
+            .finally(() => { done(); });
     });
     it("can find a created game entry", function(done) {
         createGame()
@@ -52,6 +56,10 @@ describe("Test the 'game' actions", function() {
             .then((result:GameReturn) => {
                 verifyReturn(result, "Expected a result from finding a game");
                 expect(result.first().name).toBe(game);
+            })
+            .catch(() => {
+                // fail the test
+                expect(true).toBeFalsy("Test should have succeeded");
             })
             .finally(() => { done(); });
     });
@@ -63,6 +71,10 @@ describe("Test the 'game' actions", function() {
             .then((result:GameReturn) => {
                 verifyReturn(result, "Expected a result from finding a game by name");
                 expect(result.first().name).toBe(game);
+            })
+            .catch(() => {
+                // fail the test
+                expect(true).toBeFalsy("Test should have succeeded");
             })
             .finally(() => { done(); });
     });
