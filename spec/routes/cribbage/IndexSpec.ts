60f428d596417d06e78af3ebecac99b3e9fda564
Sean Brown <smbrown3706@gmail.com>
2016-10-16 22:52:10 -0400

    Removed unused header

--- spec/routes/cribbage/IndexSpec.ts
+++ spec/routes/cribbage/IndexSpec.ts
@@ -1,6 +1,5 @@
 import {CribbageRoutes} from "../../../routes/Cribbage/index";
 import Router = CribbageRoutes.Router;
-import {BaseCard as Card, Value} from "../../../card_service/base_classes/items/card";
 
 describe("Test the cribbage routes 'index' class", function() {
     var router:Router;
