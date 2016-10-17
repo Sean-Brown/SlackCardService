b3d046218961d750ff70234330c588747471ab09
Sean Brown <smbrown3706@gmail.com>
2016-10-16 22:24:14 -0400

    Changed the create() signature to accept a CribbageHandHistory table object. Note the ID is not taken into account, the database assigns the ID, I usually just construct the objects with a default of 0. TODO just create a factory method that puts in the 0.

--- db/abstraction/interfaces/icribbage_hand_history_actions.ts
+++ db/abstraction/interfaces/icribbage_hand_history_actions.ts
@@ -1,12 +1,29 @@
 import {CribbageHandHistoryReturn} from "../return/db_return";
+import {CribbageHandHistory} from "../tables/cribbage_hand_history";
 export interface ICribbageHandHistoryActions {
     /**
-     * Create a HandHistory row in the database
-     * @param player_id the ID of the player who the hand belongs to
-     * @param game_history_id the ID of the GameHistory this hand occurred in
-     * @param hand the hand itself in the form "2s-kd-10c-8h"
-     * @param cut the cut card in short form, e.g. 10c for "ten of clubs"
+     * Create a CribbageHandHistory row in the database
+     * @param {CribbageHandHistory} chh a CribbageHandHistory object -- note the ID will be ignored
      * @return {Q.Promise<CribbageHandHistoryReturn>} a promise that will return the newly created row
      */
-    create(player_id:number, game_history_id:number, hand:string, cut:string):Q.Promise<CribbageHandHistoryReturn>;
+    create(chh:CribbageHandHistory):Q.Promise<CribbageHandHistoryReturn>;
+
+    /**
+     * Create many CribbageHandHistory rows
+     * @param chhs an array of CribbageHandHistory objects -- note the IDs will be ignored
+     */
+    createMany(chhs:Array<CribbageHandHistory>):Q.Promise<CribbageHandHistoryReturn>;
+
+    /**
+     * Remove the CribbageHandHistory records associated with the given GameHistory ID
+     * @param game_history_id
+     */
+    remove(game_history_id:number):Q.Promise<CribbageHandHistoryReturn>;
+
+    /**
+     * Get all the cribbage hands for a player in a game they played in
+     * @param player_id
+     * @param game_history_id
+     */
+    all(player_id:number, game_history_id:number):Q.Promise<CribbageHandHistoryReturn>;
 }
