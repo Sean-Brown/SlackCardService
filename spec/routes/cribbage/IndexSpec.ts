8602e5e4ab591fb8ff52fe7515a12958f9f3d30c
Sean Brown <smbrown3706@gmail.com>
2016-10-23 10:02:23 -0400

    fleshing out the spec

--- spec/routes/cribbage/IndexSpec.ts
+++ spec/routes/cribbage/IndexSpec.ts
@@ -1,9 +1,20 @@
 import {CribbageRoutes} from "../../../routes/Cribbage/index";
 import Router = CribbageRoutes.Router;
+import {deleteTables} from "../../db/postgres/integration/CreateTablesSpec";
 
 describe("Test the cribbage routes 'index' class", function() {
     var router:Router;
-    beforeEach(() => {
+    beforeEach((done) => {
+        // Create the database
         router = new Router();
+        router.init()
+            .then(() => {
+                // Mock the request and response objects
+            })
+            .finally(() => { done(); });
     });
+    afterEach((done) => {
+        // Destroy the database
+        deleteTables().finally(() => { done(); });
+    })
 });
