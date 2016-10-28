c88f986681666dcf1cd09d4e946032e14f845b27
Sean Brown <smbrown3706@gmail.com>
2016-10-27 20:42:40 -0400

    fixed format of comment

--- routes/slack.ts
+++ routes/slack.ts
@@ -2,6 +2,6 @@ export enum SlackResponseType {
     /* message sent to the user */
     ephemeral = <any>"ephemeral",
 
-        /* message sent to the channel */
+    /* message sent to the channel */
     in_channel = <any>"in_channel"
 }
